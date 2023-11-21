e2840e3e05f0dbd789bdead4a95646707515ea09
BapBoVN <Biencuong28092004@gmail.com>
2023-11-21 08:18:09 +0700

    cuong21/11/2023

--- Dự án 1/js/index.js
+++ Dự án 1/js/index.js
@@ -45,6 +45,9 @@ function showSlides(n) {
   slides[slideIndex - 1].style.display = "block";
   dots[slideIndex - 1].className += " active";
 }
+setInterval(() => {
+  plusSlides(1);
+}, 3000);
 
 
 
