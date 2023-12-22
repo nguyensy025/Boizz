3b1ea2175b059c3679ff58ca05e66da5fb6af2b6
nguyenhuudo06 <144748934+nguyenhuudo06@users.noreply.github.com>
2023-12-22 15:15:21 +0700

    do

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Log;
@@ -19,9 +21,32 @@ class UserController extends Controller
 
     public function postRegister(Request $request){
         // Chua validate
+        $request->merge(['password'=>Hash::make($request->password)]);
+
         try{
             DB::beginTransaction();
+            User::create($request->all());
 
+            DB::commit();
+        } catch(\Exception $exception){
+            DB::rollBack();
+            Log::error('Message' . $exception->getMessage() . 'Line' . $exception->getLine());
+        }
+
+        return redirect()->route('user.login');
+    }
+
+    public function portLogin(Request $request){
+        try{
+            DB::beginTransaction();
+            // dd($request->password);
+            if (Auth::attempt([
+                'email' => $request->email, 
+                'password' => $request->password
+            ])){
+                return redirect()->route('home.index');
+            }
+            // return redirect()->back()->with('error', 'Sai tài khoản hoặc mật khẩu');
 
             DB::commit();
         } catch(\Exception $exception){
